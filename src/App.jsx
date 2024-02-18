a97678983ae0a137aa145c5993ff4f3a56d1f0e9
Mueed <studymueed@gmail.com>
2024-02-18 23:25:07 +0530

    Adjust margin for the whole content and added favicon logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import NavBar from "./components/NavBar";
-import IntroSection from "./components/IntroSection";
-import Heading from "./components/Heading";
-import SlantedDiv from "./components/SlantedDiv";
-import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar.jsx";
+import IntroSection from "./components/IntroSection.jsx";
+import Heading from "./components/Heading.jsx";
+import SlantedDiv from "./components/SlantedDiv.jsx";
+import HeroSection from "./components/HeroSection.jsx";
 
 function App() {
   return (
     <>
-      <div style={{ margin: "0 100px" }}>
+      <div style={{ margin: "0 90px" }}>
         <NavBar />
         <Heading variant="h1">Make Self-Purification by Reading Quran</Heading>
         <IntroSection />
       </div>
       <SlantedDiv />
-      <div style={{ margin: "0 100px" }}>
+      <div style={{ margin: "0 90px" }}>
         <HeroSection />
         <Heading className="text" variant="h2" margin={200}>
           Start the Journey of Enlightenment
