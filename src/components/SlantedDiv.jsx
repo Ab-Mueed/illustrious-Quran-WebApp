5f311518dcaa85cc477c28ddbb118e9a9802b9db
Mueed <studymueed@gmail.com>
2024-02-19 17:31:19 +0530

    Adjusted textAlign and fontSize of second-slant in SlantedDiv.jsx

--- src/components/SlantedDiv.jsx
+++ src/components/SlantedDiv.jsx
@@ -2,21 +2,21 @@ import { Box, Typography } from "@mui/material";
 export default function SlantedDiv() {
   return (
     <>
-      <Box sx={{ width:'100vw', position:'relative'}}>
+      <Box sx={{ width: "100vw", position: "relative" }}>
         <Box className="first-slant"></Box>
         <Box className="second-slant" sx={{ alignItems: "center" }}>
           <Typography
             variant="body1"
             sx={{
               color: "#12372A",
-              fontSize: 34.7,
+              fontSize: "1.7vw",
               paddingTop: 1,
               fontWeight: 500,
+              textAlign: "center",
             }}
           >
-            ation &nbsp;&nbsp; + &nbsp;&nbsp; Timeless Wisdom &nbsp;&nbsp; +
-            &nbsp;&nbsp; Literary Excellence &nbsp;&nbsp; + &nbsp;&nbsp;
-            Universality &nbsp;&nbsp; + &nbsp;&nbsp; Comprehe
+            “Allah does not burden a soul beyond that it can bear…” (Al-Baqarah,
+            2:286)
           </Typography>
         </Box>
       </Box>
