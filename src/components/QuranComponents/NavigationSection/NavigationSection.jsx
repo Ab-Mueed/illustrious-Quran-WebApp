717ca979194afd5dc3136ad2b5f161498739f92f
Hasaan Qari <hassanqari9@gmail.com>
2024-03-07 22:16:40 +0530

    fixed not found error

--- src/components/QuranComponents/NavigationSection/NavigationSection.jsx
+++ src/components/QuranComponents/NavigationSection/NavigationSection.jsx
@@ -6,18 +6,40 @@ const tempArray = [1, 2, 3, 4, 5, 6];
 const URL = "http://api.alquran.cloud/v1/surah";
 
 const NavigationSection = () => {
-    console.log('In Quran Navigation');
+  console.log('In Quran Navigation');
   const [surah, setSurah] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchSurah = async () => {
-      const response = await fetch(`${URL}`);
-      const surah = await response.json();
-      setSurah(surah);
-    };
-    fetchSurah()
-}, []);
-console.log(surah.data);
+    try {
+      setIsLoading(true)
+      const fetchSurah = async () => {
+        const response = await fetch(`${URL}`);
+        const surah = await response.json();
+        setSurah(surah);
+      };
+      fetchSurah()
+      setIsLoading(false)
+    } catch {
+      console.log('Error while fetching data');
+    }
+  }, []);
+  
+  if (isloading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  if(!surah.data) {
+    return (
+      <div>
+        <h1>No data found</h1>
+      </div>
+    )
+  }
 
   return (
     <>
@@ -25,9 +47,9 @@ console.log(surah.data);
         <Grid item  xs={12} key={index}>
           <Button
             variant="text"
-            sx={{ width: "100%", border: 2, borderColor: "white" }}
+            sx={{ width: "100%", border: '1px solid lightgrey'}}
           >
-            <Box sx={{ display: "flex", gap: 4 }}>
+            <Box sx={{ display: "flex", gap: 2 }}>
               <Typography variant="body1">{item.number}</Typography>
               <Typography variant="body1">{item.englishName}</Typography>
               <Typography variant="body1">{item.name}</Typography>
